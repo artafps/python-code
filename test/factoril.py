4d55559088d72a7c580f9f6d302350e9a5f0e7af
arta fallahpoor <102357381+artafallahpoor@users.noreply.github.com>
2022-11-27 23:16:38 +0330

    Update factoril.py

--- test/factoril.py
+++ test/factoril.py
@@ -4,7 +4,13 @@ def factorile(y,sum=1):
         return x 
     else:
        return factorile(y-1,x)
+def factorile2(y):
+    if(y==1):
+        return 1 
+    else:
+       return factorile2(y-1) * y
 
 
 
-print(factorile(185))
+print(factorile(6))
+
